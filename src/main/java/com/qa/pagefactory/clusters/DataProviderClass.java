e14771a14b71c61765733e1f9c6746a877697db5
ajaiswal-unravel <ajaiswal@unraveldata.com>
2020-10-10 16:21:15 +0530

    Added Code doc

--- src/main/java/com/qa/pagefactory/clusters/DataProviderClass.java
+++ src/main/java/com/qa/pagefactory/clusters/DataProviderClass.java
@@ -1,17 +1,25 @@
 package com.qa.pagefactory.clusters;
+
 import java.lang.reflect.Method;
 
+/**
+ * @author Ankur Jaiswal
+ * This class contains data provider methods which will provide data to
+ * the test cases on run time.
+ */
 public class DataProviderClass {
-
-  public static Object[][] getClusterIdsForClusterType(Method method) {
-    if (method.getDeclaringClass().getPackage().getName().contains("impala")) {
-      return new Object[][] {{"tnode54(CM)-wnode54(U)-TLS-Kerb-Sentry"}};
-      //return new Object[][] {{"tnode54(CM)-wnode54(U)-TLS-Kerb-Sentry"}, {"tnode57(CM)-wnode26(U)-TLS-Kerb-Sentry"}};
-      //return new Object[][] {{"Cluster1"}};
+    /**
+     * This method returns cluster ids on which test cases will be executed
+     * @param method - Reflection @test method
+     * @return - List of clusterIds
+     */
+    public static Object[][] getClusterIdsForClusterType(Method method) {
+        /* @TODO - Need to remove hard coded values from cluster Ids this should be parameterized
+           or it should be pulled from APIs. */
+        if (method.getDeclaringClass().getPackage().getName().contains("impala")) {
+            return new Object[][]{{"tnode54(CM)-wnode54(U)-TLS-Kerb-Sentry"}};
+        } else {
+            return new Object[][]{{"wnode25-HDP315-TLS-Kerb-Ranger"}, {"wnode26-CDH633-TLS-Kerb-Sentry"}};
+        }
     }
-    else {
-      return new Object[][] {{"wnode25-HDP315-TLS-Kerb-Ranger"},{"wnode26-CDH633-TLS-Kerb-Sentry"}};
-      //return new Object[][] {{"Cluster1 [296b...2ac9]"},{"Cluster1 [8e83...8441]"}};
-    }
-  }
 }

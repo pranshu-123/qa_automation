292bd30b8e06ff02724d9abb3bf69275de6e2d50
Birender-un <bkumar@unraveldata.com>
2020-10-16 00:03:32 +0530

    removed invalid import package com.sun.xml.internal.ws.model

--- src/main/java/com/qa/pagefactory/manage/ManagePageObject.java
+++ src/main/java/com/qa/pagefactory/manage/ManagePageObject.java
@@ -1,12 +1,10 @@
 package com.qa.pagefactory.manage;
 
-import com.sun.xml.internal.ws.model.WrapperBeanGenerator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import javax.swing.text.html.CSS;
 import java.util.List;
 
 /**

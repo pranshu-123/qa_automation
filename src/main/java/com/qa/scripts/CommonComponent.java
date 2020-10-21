2390267c0863a2c99296c5d98f7391b2ea5ab063
ajaiswal-unravel <ajaiswal@unraveldata.com>
2020-10-21 20:25:31 +0530

    Added comments in CommonComponent

--- src/main/java/com/qa/scripts/CommonComponent.java
+++ src/main/java/com/qa/scripts/CommonComponent.java
@@ -4,8 +4,16 @@ import com.qa.pagefactory.CommonPageObject;
 import com.qa.utils.MouseActions;
 import org.openqa.selenium.WebDriver;
 
+/**
+ * @author Ankur Jaiswal
+ * This is common class used across all components
+ */
 public class CommonComponent {
 
+  /**
+   * This method with close the pop up modal if exist on web page
+   * @param driver WebDriver instance
+   */
   public static void closeModalIfExists(WebDriver driver) {
     CommonPageObject commonPageObject = new CommonPageObject(driver);
     if (commonPageObject.closeModalButton.size() > 0) {

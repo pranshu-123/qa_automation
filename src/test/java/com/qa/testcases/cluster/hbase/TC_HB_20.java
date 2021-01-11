9f4882832b68dfcb9e19c538a41922f141cb1091
Birender-un <bkumar@unraveldata.com>
2021-01-11 17:12:22 +0530

    added cluster HBase testcases TC_HB_19,20

---
+++ src/test/java/com/qa/testcases/cluster/hbase/TC_HB_20.java
@@ -0,0 +1,55 @@
+package com.qa.testcases.cluster.hbase;
+
+import com.qa.annotations.Marker;
+import com.qa.base.BaseClass;
+import com.qa.pagefactory.clusters.HBasePageObject;
+import com.qa.scripts.clusters.HBasePage;
+import com.qa.utils.Log;
+import com.qa.utils.MouseActions;
+import com.qa.utils.WaitExecuter;
+import com.relevantcodes.extentreports.LogStatus;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+@Marker.ClusterHBase
+@Marker.All
+public class TC_HB_20 extends BaseClass {
+
+    private static final Logger LOGGER = Logger.getLogger(TC_HB_20.class.getName());
+
+    @Test(dataProvider = "clusterid-data-provider")
+    public void verifyHBaseClusterNameBySelectingCluster(String clusterId) {
+        test = extent.startTest("TC_HB_20.verifyHBaseClusterNameBySelectingCluster",
+                "Verify Cluster name should be listed as tab with cluster details.");
+        test.assignCategory(" Cluster - HBasePage ");
+        Log.startTestCase("TC_HB_20.verifyHBaseClusterNameBySelectingCluster");
+
+        // Initialize all classes objects
+        LOGGER.info("Initialize all class objects");
+        WaitExecuter waitExecuter = new WaitExecuter(driver);
+        HBasePage hbase = new HBasePage(driver);
+        HBasePageObject hBasePageObject = new HBasePageObject(driver);
+
+        //Navigate to HBase tab
+        waitExecuter.waitUntilElementClickable(hBasePageObject.hbaseTab);
+        MouseActions.clickOnElement(driver,hBasePageObject.hbaseTab);
+        LOGGER.info("Clicked on HBase header");
+        waitExecuter.waitUntilElementPresent(hBasePageObject.hbaseHeader);
+        LOGGER.info("HBase headers found: "+ hbase.getHBaseHeader());
+
+        String hBaseClusterName = "HDPab722";
+        hbase.selectHBaseCluster(hBaseClusterName);
+        String hBaseNameActual = hbase.getHBaseHeader();
+        String hBaseNameExpected = "HBase (HDPab722)";
+        LOGGER.info("Cluster name found with cluster details: "+hBaseNameActual);
+        Assert.assertTrue(hBaseNameActual.contains(hBaseClusterName), "HBase actual header do not contains " +
+                "hbase cluster name");
+        Assert.assertEquals(hBaseNameActual, hBaseNameExpected, "Not matched actual HBase name:"+hBaseNameActual
+                +" with expected: "+ hBaseNameExpected);
+        test.log(LogStatus.PASS, "Verified Cluster name should be listed as tab with cluster details.");
+
+    }
+}

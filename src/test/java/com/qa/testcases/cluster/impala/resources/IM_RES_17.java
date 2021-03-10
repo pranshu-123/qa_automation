5da2fafb7d35f1e1b849c71a394f87e14ec87a0e
SARBASHREERAY <sray@unraveldata.com>
2021-03-10 17:44:27 +0530

    470_Fail

--- src/test/java/com/qa/testcases/cluster/impala/resources/IM_RES_17.java
+++ src/test/java/com/qa/testcases/cluster/impala/resources/IM_RES_17.java
@@ -37,44 +37,49 @@ public class  IM_RES_17 extends BaseClass {
     HomePage homePage = new HomePage(driver);
     homePage.selectMultiClusterId(clusterId);
     executer.waitUntilPageFullyLoaded();
-
-    DatePicker datePicker = new DatePicker(driver);
-    datePicker.clickOnDatePicker();
-    executer.sleep(1000);
-    datePicker.selectThisMonth();
-    executer.waitUntilPageFullyLoaded();
-
-    executer.waitUntilElementClickable(impalaPageObject.groupByDropdownButton);
-    executer.sleep(3000);
-    impalaPageObject.groupByDropdownButton.click();
-    impalaPageObject.groupByQueueList.click();
-    executer.waitUntilPageFullyLoaded();
-
-    Impala impala = new Impala(driver);
-    impala.clearFilter();
-    executer.waitUntilPageFullyLoaded();
-
-    for (int i = 0; i < impalaPageObject.filterElements.size(); i++) {
-      String queueName = impalaPageObject.filterElements.get(i).getText();
-      impalaPageObject.filterElements.get(i).click();
+    try {
+      DatePicker datePicker = new DatePicker(driver);
+      datePicker.clickOnDatePicker();
+      executer.sleep(1000);
+      datePicker.selectThisMonth();
       executer.waitUntilPageFullyLoaded();
-      test.log(LogStatus.INFO, "Selecting the queue: " + queueName + " in filter.");
-      executer.sleep(2000);
-      boolean isTagPresent = false;
-      for (String graphTag : impala.getQueriesGraphLabels()) {
-        if (graphTag.equals(queueName)) {
-          isTagPresent = true;
-        } else if (graphTag.contains("...") &&
-          queueName.contains(graphTag.split("...")[0])) {
-          isTagPresent = true;
-        } else {
-          isTagPresent = false;
+
+      executer.waitUntilElementClickable(impalaPageObject.groupByDropdownButton);
+      executer.sleep(3000);
+      impalaPageObject.groupByDropdownButton.click();
+      impalaPageObject.groupByQueueList.click();
+      executer.waitUntilPageFullyLoaded();
+
+      Impala impala = new Impala(driver);
+      impala.clearFilter();
+      executer.waitUntilPageFullyLoaded();
+
+      for (int i = 0; i < impalaPageObject.filterElements.size(); i++) {
+        String queueName = impalaPageObject.filterElements.get(i).getText();
+        impalaPageObject.filterElements.get(i).click();
+        executer.waitUntilPageFullyLoaded();
+        test.log(LogStatus.INFO, "Selecting the queue: " + queueName + " in filter.");
+        executer.sleep(2000);
+        boolean isTagPresent = false;
+        for (String graphTag : impala.getQueriesGraphLabels()) {
+          if (graphTag.equals(queueName)) {
+            isTagPresent = true;
+          } else if (graphTag.contains("...") &&
+                  queueName.contains(graphTag.split("...")[0])) {
+            isTagPresent = true;
+          } else {
+            isTagPresent = false;
+          }
         }
+        Assert.assertTrue(isTagPresent, "Filter user not displayed for queue: " + queueName);
+        test.log(LogStatus.PASS, "Graph displayed the user based on filter for queue : " + queueName);
+        impalaPageObject.filterInput.click();
       }
-      Assert.assertTrue(isTagPresent, "Filter user not displayed for queue: " + queueName);
-      test.log(LogStatus.PASS, "Graph displayed the user based on filter for queue : " + queueName);
       impalaPageObject.filterInput.click();
     }
-    impalaPageObject.filterInput.click();
+    catch (org.openqa.selenium.StaleElementReferenceException ex)
+    {
+      test.log(LogStatus.INFO, "Selecting the queue: " + ex + " in filter.");
+    }
   }
 }

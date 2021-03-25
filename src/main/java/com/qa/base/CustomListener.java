e0c348da6ab38624e126808f844a325bb379e8e5
ajaiswal-unravel <ajaiswal@unraveldata.com>
2021-03-25 08:23:16 +0530

    Changes for Influx

--- src/main/java/com/qa/base/CustomListener.java
+++ src/main/java/com/qa/base/CustomListener.java
@@ -1,16 +1,26 @@
 package com.qa.base;
 
+import com.qa.connections.db.InfluxDBClient;
+import com.qa.constants.ConfigConstants;
 import com.relevantcodes.extentreports.LogStatus;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Ankur Jaiswal
  * Implementation of custom listener for testng.
  */
 public class CustomListener extends BaseClass implements ITestListener {
-
+//    private Long START;
+//    private Long END;
+    private static final Long EXECUTION_ID = System.currentTimeMillis();
+    private InfluxDBClient influxDBClient = InfluxDBClient.getConnection();
     /**
      * Add skip test cases to html report
      * @param result - Result Object of test execution
@@ -18,23 +28,65 @@ public class CustomListener extends BaseClass implements ITestListener {
     @Override
     public void onTestSkipped(ITestResult result) {
         test = extent.startTest(result.getMethod().getMethodName());
-        LogStatus sta = test.getRunStatus();
         test.log(LogStatus.SKIP, result.getTestName() + " skipped because " +
                 result.getThrowable().getMessage());
         extent.endTest(test);
         extent.flush();
+        try {
+            influxDBClient.writeDataToInflux(dataToPush(result,
+                    "SKIP"));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void onTestStart(ITestResult result) {}
+    public void onTestStart(ITestResult result) {
+//        START = System.currentTimeMillis();
+    }
+
     @Override
-    public void onTestSuccess(ITestResult result) {}
+    public void onTestSuccess(ITestResult result) {
+        try {
+            influxDBClient.writeDataToInflux(dataToPush(result,
+                    "PASS"));
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
-    public void onTestFailure(ITestResult result) {}
+    public void onTestFailure(ITestResult result) {
+        try {
+            if (result.getThrowable() instanceof AssertionError) {
+                influxDBClient.writeDataToInflux(dataToPush(result,
+                        "FAIL"));
+            } else {
+                influxDBClient.writeDataToInflux(dataToPush(result,
+                        "FATAL"));
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
     @Override
     public void onStart(ITestContext context) {}
     @Override
-    public void onFinish(ITestContext context) {}
+    public void onFinish(ITestContext context) {
+    }
+
+    private Map<String, Object> dataToPush(ITestResult result,
+                                   String status) throws UnknownHostException {
+        Map<String, Object> data = new HashMap<>();
+        data.put("test_case_id", result.getMethod().getMethodName());
+        data.put("method_name", result.getMethod().getMethodName());
+        data.put("status", status);
+        data.put("batch_id", EXECUTION_ID);
+        data.put("url", "tnode47.unraveldata.com");
+        data.put("host", InetAddress.getLocalHost().getHostName());
+        data.put("duration", result.getEndMillis() - result.getStartMillis());
+        data.put("markers", System.getProperty(ConfigConstants.SystemConfig.MARKERS));
+        return data;
+    }
 }
---
+++ src/main/java/com/qa/connections/db/InfluxDBClient.java
@@ -0,0 +1,50 @@
+package com.qa.connections.db;
+
+import com.qa.constants.FileConstants;
+import com.qa.io.InfluxConfigReader;
+import org.influxdb.BatchOptions;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+public class InfluxDBClient {
+    private static InfluxDB influxDB;
+    private static InfluxDBClient influxDBClient;
+
+    private InfluxDBClient() {
+        InfluxConfigReader configReader = new InfluxConfigReader();
+        try {
+            Map<String, Object> config = configReader.readYamlFile(FileConstants.getInfluxConfigYaml());
+            influxDB = InfluxDBFactory.connect(((Map)config.get("influx")).get("url").toString());
+            influxDB.setDatabase(((Map)config.get("influx")).get("db").toString());
+            influxDB.enableBatch(BatchOptions.DEFAULTS);
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static InfluxDBClient getConnection() {
+        if (influxDBClient == null) {
+            influxDBClient = new InfluxDBClient();
+        }
+        return influxDBClient;
+    }
+
+    public void writeDataToInflux(Map<String, Object> data) {
+        influxDB.write(Point.measurement("test_execution1")
+                .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                .tag("test_case_id", data.get("test_case_id").toString())
+                .tag("url", data.get("url").toString())
+                .tag("batch_id", data.get("batch_id").toString())
+                .addField("method_name", data.get("method_name").toString())
+                .tag("host", data.get("host").toString())
+                .tag("status", data.get("status").toString())
+                .addField("duration", data.get("duration").toString())
+                .tag("markers", data.get("markers").toString())
+                .build());
+    }
+}

373ecd269c9d240e44c5d566499d015f3ae09d0d
Sarbashree Ray <sray@unraveldata.com>
2021-04-15 16:44:18 +0530

    4700 Failed testcases

--- src/test/java/com/qa/testcases/data/fileReports/TC_FR_36.java
+++ src/test/java/com/qa/testcases/data/fileReports/TC_FR_36.java
@@ -5,6 +5,7 @@ import com.qa.base.BaseClass;
 import com.qa.pagefactory.data.FileReportsPageObject;
 import com.qa.scripts.data.FileReports;
 import com.qa.utils.MouseActions;
+import com.qa.utils.WaitExecuter;
 import com.relevantcodes.extentreports.LogStatus;
 import org.testng.annotations.Test;
 
@@ -20,9 +21,11 @@ public class TC_FR_36 extends BaseClass {
     test.assignCategory(" Data - FileReports ");
 
     FileReports filereports = new FileReports(driver);
+    WaitExecuter waitExecuter = new WaitExecuter(driver);
     FileReportsPageObject fileReportsPageObject = new FileReportsPageObject(driver);
 
     filereports.navidateTofileReportsTab();
+    waitExecuter.waitUntilElementPresent(fileReportsPageObject.mediumFile);
     MouseActions.clickOnElement(driver, fileReportsPageObject.mediumFile);
     filereports.verifyFilePathSearchOption("MEDIUM", clusterId);
     test.log(LogStatus.PASS, "Successfully validate the search a particular file path 'Medium' file report.");

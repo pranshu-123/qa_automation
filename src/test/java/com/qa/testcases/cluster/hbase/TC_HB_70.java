ad48412fd066695ade5b3fd3050914cfa32e6d3d
Sarbashree Ray <sray@unraveldata.com>
2021-05-27 17:04:25 +0530

    Issue with testcases

--- src/test/java/com/qa/testcases/cluster/hbase/TC_HB_70.java
+++ src/test/java/com/qa/testcases/cluster/hbase/TC_HB_70.java
@@ -45,7 +45,6 @@ public class TC_HB_70 extends BaseClass {
         LOGGER.info("HBase clusters found are: "+ hBaseClusters);
         Assert.assertFalse(hBaseClusters.isEmpty(), "HBase clusters not available");
 
-        boolean flag = false;
         for (String clusterName : hBaseClusters) {
             if (clusterName.contains("HDP")) {
                 LOGGER.info("Verified Multicluster setup with one HDP cluster."+clusterName);

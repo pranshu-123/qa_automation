17ead8d7c6ab895b701f3c873fb2b57655700593
Sarbashree Ray <sray@unraveldata.com>
2021-07-04 21:18:57 +0530

    Fixed Impala resources

--- src/main/java/com/qa/pagefactory/CommonPageObject.java
+++ src/main/java/com/qa/pagefactory/CommonPageObject.java
@@ -18,10 +18,10 @@ public class CommonPageObject {
   @FindBy(xpath = "//li[contains(@class,'select2-results__option')]")
   public List<WebElement> clustersList;
 
-  @FindBy(xpath = "//span[contains(@class,'select2-selection__arrow')]")
+  @FindBy(xpath = "(//span[@role='presentation'])[2]")
   public WebElement clusterDropdown;
 
-  @FindBy(xpath="(//input[@class='select2-search__field'])[2]")
+  @FindBy(xpath="//span[contains(@class, 'select2-search--dropdown')]/input")
   public WebElement clusterSearchBox;
 
   @FindBy(xpath="//span[@class='select2-results']//li")

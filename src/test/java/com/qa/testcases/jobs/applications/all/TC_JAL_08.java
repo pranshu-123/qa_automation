63a52694752ded8e4caf7b3955382a3fee81ea1b
opandey-unravel <opandey@unraveldata.com>
2021-07-05 12:08:55 +0530

    Added impala changes

--- src/test/java/com/qa/testcases/jobs/applications/all/TC_JAL_08.java
+++ src/test/java/com/qa/testcases/jobs/applications/all/TC_JAL_08.java
@@ -32,7 +32,7 @@ public class TC_JAL_08 extends BaseClass {
         DatePicker datePicker = new DatePicker(driver);
         // Navigate to Jobs tab from header
         test.log(LogStatus.INFO, "Navigate to jobs tab from header");
-        test.log(LogStatus.INFO, "Select last 7 days");
+        test.log(LogStatus.INFO, "Select custom range in datepicker");
         test.log(LogStatus.INFO, "Select clusterId : " + clusterId);
         allApps.navigateToJobsTab();
         waitExecuter.waitUntilElementClickable(applicationsPageObject.resetButton);
@@ -45,19 +45,24 @@ public class TC_JAL_08 extends BaseClass {
         waitExecuter.waitUntilElementClickable(applicationsPageObject.resetButton);
         // Get total app counts before applying filter
         waitExecuter.sleep(1000);
-        String totalCountBeforeFilter = applicationsPageObject.getTotalAppCount.getText();
 
+        String totalCountBeforeFilter = applicationsPageObject.getTotalAppCount.getText();
         test.log(LogStatus.INFO, "Get total app counts before applying filter " + totalCountBeforeFilter);
         LOGGER.info("Get total app counts before applying filter " + totalCountBeforeFilter);
+        // Select cluster
+        test.log(LogStatus.INFO, "Select clusterid : " + clusterId);
+        LOGGER.info("Select clusterId : " + clusterId);
+        allApps.selectCluster(clusterId);
+        waitExecuter.waitUntilElementClickable(applicationsPageObject.resetButton);
         // To apply filter - De-select all application types
         test.log(LogStatus.INFO, "To apply filter - De-select all application types");
         LOGGER.info("To apply filter - De-select all application types");
         allApps.deselectAllAppTypes();
+        waitExecuter.waitUntilElementClickable(applicationsPageObject.resetButton);
         waitExecuter.sleep(2000);
         // After de-selecting app type check the app counts
         int totalCountAfterFilter = Integer
                 .parseInt(applicationsPageObject.getTotalAppCount.getText().replaceAll("[^\\dA-Za-z ]", "").trim());
-        waitExecuter.sleep(2000);
         test.log(LogStatus.INFO, "After de-selecting app type check the app counts " + totalCountAfterFilter);
         LOGGER.info("After de-selecting app type check the app counts " + totalCountAfterFilter);
         Assert.assertEquals(totalCountAfterFilter, 0,
@@ -67,11 +72,7 @@ public class TC_JAL_08 extends BaseClass {
         LOGGER.info("Click on reset button");
         applicationsPageObject.resetButton.click();
         waitExecuter.sleep(2000);
-        // Select cluster
-        test.log(LogStatus.INFO, "Select clusterid : " + clusterId);
-        LOGGER.info("Select clusterId : " + clusterId);
-        allApps.selectCluster(clusterId);
-        waitExecuter.sleep(3000);
+
         // Get total app count after clicking on reset button
         String totalCountAfterReset = applicationsPageObject.getTotalAppCount.getText();
         waitExecuter.sleep(2000);

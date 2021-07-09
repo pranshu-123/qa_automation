f383a44daa20777253521d9661c20ac700d2aa5b
ajaiswal-unravel <ajaiswal@unraveldata.com>
2021-07-09 19:41:28 +0530

    Fixed

--- src/test/java/com/qa/testcases/migration/cloud_mapping_per_host/TC_CMP_93.java
+++ src/test/java/com/qa/testcases/migration/cloud_mapping_per_host/TC_CMP_93.java
@@ -65,12 +65,7 @@ public class TC_CMP_93 extends BaseClass {
             cloudMigrationPerHostPage.setCustomCost(element, "1");
             break;
         }
-
-<<<<<<< HEAD
         cloudMigrationPerHostPage.clickOnModalRunButton();
-=======
-        cloudMigrationPerHostPage.clickOnRunReportButton();
->>>>>>> 558f742f8c49fe10dacf959cb4050206a31673f4
         cloudMigrationPerHostPage.waitTillLoaderPresent();
         waitExecuter.sleep(10000);
 

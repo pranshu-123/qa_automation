0d67f295b612b1dfd15eee690aca1711c495ef7b
Sarbashree Ray <sray@unraveldata.com>
2021-08-28 13:24:31 +0530

    Fixed 4720 Fix

--- src/main/java/com/qa/pagefactory/data/FileReportsPageObject.java
+++ src/main/java/com/qa/pagefactory/data/FileReportsPageObject.java
@@ -8,77 +8,76 @@ import org.openqa.selenium.support.PageFactory;
 import java.util.List;
 
 public class FileReportsPageObject {
-  @FindBy(xpath = "//span[@class='select2-selection__rendered']")
-  public WebElement clusterDropDown;
+    @FindBy(xpath = "//span[@class='select2-selection__rendered']")
+    public WebElement clusterDropDown;
 
-  @FindBy(xpath = "//ul[@class='select2-results__options']/li")
-  public List<WebElement> clusterList;
+    @FindBy(xpath = "//ul[@class='select2-results__options']/li")
+    public List<WebElement> clusterList;
 
-  @FindBy(xpath = "//a[contains(text(),'LARGE')]")
-  public WebElement largeFile;
+    @FindBy(xpath = "//a[contains(text(),'LARGE')]")
+    public WebElement largeFile;
 
-  @FindBy(xpath = "//a[contains(@class,'icon-sort-sorted-up')]")
-  public WebElement sortUp;
+    @FindBy(xpath = "//a[contains(@class,'icon-sort-sorted-up')]")
+    public WebElement sortUp;
 
-  @FindBy(xpath = "//a[contains(@class,'icon-sort-sorted-down')]")
-  public WebElement sortDown;
+    @FindBy(xpath = "//a[contains(@class,'icon-sort-sorted-down')]")
+    public WebElement sortDown;
 
-  @FindBy(xpath = "//span[normalize-space()='Avg File Size']")
-  public WebElement sortAvgFileSize;
+    @FindBy(xpath = "//span[normalize-space()='Avg File Size']")
+    public WebElement sortAvgFileSize;
 
+    @FindBy(xpath = "//button[text()[normalize-space()='LARGE']]")
+    public WebElement selectedLargeFileOpt;
 
-  @FindBy(xpath = "//button[text()[normalize-space()='LARGE']]")
-  public WebElement selectedLargeFileOpt;
+    @FindBy(xpath = "//button[contains(text(),'MEDIUM')]")
+    public WebElement mediumFile;
 
-  @FindBy(xpath = "//a[contains(text(),'MEDIUM')]")
-  public WebElement mediumFile;
+    @FindBy(xpath = "//button[text()[normalize-space()='MEDIUM']]")
+    public WebElement selecteMediumFileOpt;
 
-  @FindBy(xpath = "//button[text()[normalize-space()='MEDIUM']]")
-  public WebElement selecteMediumFileOpt;
+    @FindBy(xpath = "//button[contains(text(),'TINY')]")
+    public WebElement tinyFile;
 
-  @FindBy(xpath = "//a[contains(text(),'TINY')]")
-  public WebElement tinyFile;
+    @FindBy(xpath = "//button[text()[normalize-space()='TINY']]")
+    public WebElement selecteTinyFileOpt;
 
-  @FindBy(xpath = "//button[text()[normalize-space()='TINY']]")
-  public WebElement selecteTinyFileOpt;
+    @FindBy(xpath = "//button[contains(text(),'EMPTY')]")
+    public WebElement emptyFile;
 
-  @FindBy(xpath = "//a[contains(text(),'EMPTY')]")
-  public WebElement emptyFile;
+    @FindBy(xpath = "//button[text()[normalize-space()='EMPTY']]")
+    public WebElement selecteEmptyFileOpt;
 
-  @FindBy(xpath = "//button[text()[normalize-space()='EMPTY']]")
-  public WebElement selecteEmptyFileOpt;
+    @FindBy(xpath = "//div[@class='col-md-2']/input")
+    public WebElement searchField;
 
-  @FindBy(xpath = "//div[@class='col-md-2']/input")
-  public WebElement searchField;
+    @FindBy(xpath = "//table[@class='component-data-tables row-hover']/thead/tr/th")
+    public List<WebElement> tableHeader;
 
-  @FindBy(xpath = "//table[@class='component-data-tables row-hover']/thead/tr/th")
-  public List<WebElement> tableHeader;
+    @FindBy(xpath = "//table[@class='component-data-tables row-hover']/tbody/tr")
+    public List<WebElement> fileTableRows;
 
-  @FindBy(xpath = "//table[@class='component-data-tables row-hover']/tbody/tr")
-  public List<WebElement> fileTableRows;
+    @FindBy(xpath = "//table/thead/tr")
+    public WebElement colData;
 
-  @FindBy(xpath = "//table/thead/tr")
-  public WebElement colData;
+    @FindBy(xpath = "//table[@class='component-data-tables row-hover']/tbody")
+    public WebElement rowData;
 
-  @FindBy(xpath = "//table[@class='component-data-tables row-hover']/tbody")
-  public WebElement rowData;
+    @FindBy(xpath = "//h3[normalize-space()='No Data Available']")
+    public WebElement noDataText;
 
-  @FindBy(xpath = "//h3[normalize-space()='No Data Available']")
-  public WebElement noDataText;
+    @FindBy(xpath = "//*[@id='file-report']//div[4]/div[2]/nav/p")
+    public WebElement pagination;
 
-  @FindBy(xpath = "//*[@id='file-report']//div[4]/div[2]/nav/p")
-  public WebElement pagination;
+    @FindBy(xpath = "//*[name()='svg' and @data-icon='caret-right']")
+    public WebElement rightCaretReportCnt;
 
-  @FindBy(xpath = "//*[name()='svg' and @data-icon='caret-right']")
-  public WebElement rightCaretReportCnt;
+    @FindBy(xpath = "//*[name()='svg' and @data-icon='backward']")
+    public WebElement backwardCaretReportCnt;
 
-  @FindBy(xpath = "//*[name()='svg' and @data-icon='backward']")
-  public WebElement backwardCaretReportCnt;
-
-  /**
-   * @param driver The driver that will be used to look up the elements
-   */
-  public FileReportsPageObject(WebDriver driver) {
-    PageFactory.initElements(driver, this);
-  }
+    /**
+     * @param driver The driver that will be used to look up the elements
+     */
+    public FileReportsPageObject(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
 }

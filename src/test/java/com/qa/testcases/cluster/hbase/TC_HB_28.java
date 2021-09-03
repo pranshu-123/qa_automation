4568fb4014c11db8f64834fcdc69db8836fe84a9
Pranshu Agarwal <pagarwal@unraveldata.com>
2021-09-03 21:42:33 +0530

    all in one

--- src/test/java/com/qa/testcases/cluster/hbase/TC_HB_28.java
+++ src/test/java/com/qa/testcases/cluster/hbase/TC_HB_28.java
@@ -33,7 +33,7 @@ public class TC_HB_28 extends BaseClass {
 
         //Navigate to HBase tab
         waitExecuter.waitUntilElementClickable(hBasePageObject.hbaseTab);
-        waitExecuter.sleep(2000);
+        waitExecuter.sleep(3000);
         MouseActions.clickOnElement(driver, hBasePageObject.hbaseTab);
         LOGGER.info("Clicked on HBase Tab");
         hbase.selectDateAsLast30Days();

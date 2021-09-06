68da8547cac7d3585e99da2aae9b1ff34562f5c0
Sarbashree Ray <sray@unraveldata.com>
2021-09-06 12:14:08 +0530

    Fixed 4720 FR  Fix

--- src/test/java/com/qa/testcases/appdetails/impala/IM_TC_07_Part2.java
+++ src/test/java/com/qa/testcases/appdetails/impala/IM_TC_07_Part2.java
@@ -44,9 +44,9 @@ public class IM_TC_07_Part2 extends BaseClass {
             allApps.selectCluster(clusterId);
             datePicker.clickOnDatePicker();
             datePicker.selectLast90Days();
-            waitExecuter.waitUntilElementClickable(appDetailsPageObject.resetButton);
+            waitExecuter.waitUntilPageFullyLoaded();
             int appCount = appDetailsPage.clickOnlyLink("Impala");
-            waitExecuter.waitUntilElementClickable(appDetailsPageObject.resetButton);
+            waitExecuter.waitUntilPageFullyLoaded();
             appDetailsPage.selectSuccessfulApplication();
             parentCondition:
             if (appCount > 0) {

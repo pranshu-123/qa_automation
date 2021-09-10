a13b0627297f34897a0418a691d0a42c0adf2345
Sarbashree Ray <sray@unraveldata.com>
2021-09-10 16:12:34 +0530

    Fixed 4720 Spark Fix

--- src/test/java/com/qa/testcases/appdetails/spark/TC_spark_221.java
+++ src/test/java/com/qa/testcases/appdetails/spark/TC_spark_221.java
@@ -64,7 +64,7 @@ public class TC_spark_221 extends BaseClass {
             test.log(LogStatus.PASS, "The Failed apps have all kpis listed along with components present");
         } else {
             //Close apps details page
-            MouseActions.clickOnElement(driver, sparkPageObj.closeAppsPageTab);
+            MouseActions.clickOnElement(driver, sparkPageObj.homeTab);
             test.log(LogStatus.SKIP, "No Spark Application present");
             logger.error("No Spark Application present in the " + clusterId + " cluster for the time span " +
                 "of 90 days");

cab3ceca48eeb6c833b936de48ecb1e8456fc15c
pagarwal-unravel <pagarwal@unraveldata.com>
2021-10-12 21:10:25 +0530

    TC_CMP_11 added

---
+++ src/test/java/com/qa/testcases/migration/cloud_mapping_per_host/TC_CMP_11.java
@@ -0,0 +1,49 @@
+package com.qa.testcases.migration.cloud_mapping_per_host;
+
+
+import com.qa.annotations.Marker;
+import com.qa.base.BaseClass;
+import com.qa.enums.migration.MigrationCloudMappingModalTable;
+import com.qa.scripts.migration.CloudMigrationPerHostPage;
+import com.qa.utils.LoggingUtils;
+import com.relevantcodes.extentreports.LogStatus;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import com.qa.pagefactory.migration.CloudMappingPerHostPageObject;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Marker.CloudMappingPerHost
+@Marker.All
+public class TC_CMP_11 extends BaseClass {
+
+    private static final LoggingUtils LOGGER = new LoggingUtils(TC_CMP_11.class);
+
+    @Test
+    public void verifyEMRCostEffectivenessWithObjectStorage() {
+        test = extent.startTest("TC_CMP_11.verifyEMRCostEffectivenessWithObjectStorage", "Verify Unravel recommends the best EMR instance based on max capacity for \"Lift and Shift\" and cluster node usage for \"cost reduction\" when 5 of the instances in Run/Schedule are selected. Make sure that Unravel recommends the most cost effective instance. " +
+                "for Object Storage");
+
+        CloudMappingPerHostPageObject cloudMappingPerHostPageObject = new CloudMappingPerHostPageObject(driver);
+        test.assignCategory("Migration/Cloud Mapping Per Host");
+        CloudMigrationPerHostPage cloudMigrationPerHostPage = new CloudMigrationPerHostPage(driver);
+        LOGGER.info("Navigate to migration host page", test);
+        cloudMigrationPerHostPage.navigateToCloudMappingPerHost();
+        LOGGER.info("Click on Run button", test);
+        cloudMigrationPerHostPage.clickOnRunButton();
+        cloudMigrationPerHostPage.waitTillLoaderPresent();
+        LOGGER.info("Select Object storage.", test);
+        cloudMigrationPerHostPage.selectStorage("Object storage");
+        cloudMigrationPerHostPage.waitTillLoaderPresent();
+        List<String> value1 = cloudMigrationPerHostPage.getColumnValuesFromModalTable(MigrationCloudMappingModalTable.COST);
+        System.out.println(Arrays.toString(value1.toArray()));
+//        cloudMigrationPerHostPage.setCustomCost(MigrationCloudMappingModalTable.CUSTOM_COST,"41");
+
+        for (WebElement element : cloudMappingPerHostPageObject.modalTableRows) {
+            cloudMigrationPerHostPage.setCustomCost(element, "1");
+        }
+        test.log(LogStatus.PASS, "Validated EMR recommends the most effective instance with Object Storage selected successfully.");
+    }
+}

96b79a55e2a3424e3001ed6cbafa12431d9d0f90
Pranshu <pagarwal@unraveldata.com>
2021-11-12 11:29:50 +0530

    removed unused object

--- src/test/java/com/qa/testcases/cluster/elk/TC_ELK_25.java
+++ src/test/java/com/qa/testcases/cluster/elk/TC_ELK_25.java
@@ -33,7 +33,6 @@ public class TC_ELK_25 extends BaseClass {
 	LOGGER.info("Initialize all class objects");
 	ELKPage elkPage = new ELKPage(driver);
 	ELKPageObject elkPageObj = new ELKPageObject(driver);
-	KafkaPage kafkaPage = new KafkaPage(driver);
 	KafkaPageObject kafkaPageObject = new KafkaPageObject(driver);
 	WaitExecuter waitExecuter = new WaitExecuter(driver);
 

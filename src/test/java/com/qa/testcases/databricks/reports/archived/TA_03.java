6ab215b0b5dbc0ac5eb947e071368d2a75421d0e
pagarwal-unravel <87324209+pagarwal-unravel@users.noreply.github.com>
2022-03-22 12:33:33 +0530

    Update TA_03.java

--- src/test/java/com/qa/testcases/databricks/reports/archived/TA_03.java
+++ src/test/java/com/qa/testcases/databricks/reports/archived/TA_03.java
@@ -1,14 +1,14 @@
 package com.qa.testcases.databricks.reports.archived;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Properties;
 import java.util.logging.Logger;
+
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
 import com.qa.annotations.Marker;
 import com.qa.base.BaseClass;
-import com.qa.scripts.databricks.reports.ArchivedReports;
+import com.qa.io.ConfigReader;
 import com.qa.scripts.databricks.reports.TopXReports;
 import com.qa.utils.Log;
 import com.relevantcodes.extentreports.LogStatus;
@@ -25,6 +25,8 @@ public class TA_03 extends BaseClass
 		Log.startTestCase("AR_Reports_03_ValidateCopyUrlFunctionality");
 		TopXReports topXReports = new TopXReports(driver);
 		topXReports.navigateToDifferentReportsTab("TopX");
+		Properties prop = ConfigReader.readBaseConfig();
+		String url = prop.getProperty("url");
 		LOGGER.info("Navigated to Reports tab");
 		topXReports.selectRun();
 		 topXReports.createNewReportWithTags("3","Name","ML team");
@@ -32,9 +34,9 @@ public class TA_03 extends BaseClass
 		topXReports.navigateToDifferentReportsTab("Archived");
 		LOGGER.info("Navigated to Reports tab");
 		String copiedUrl = topXReports.copyUrlAndNavigate();
-		Assert.assertTrue(copiedUrl.contains("https://playground-databricks4730.unraveldata.com/#/reports/topx?report_id="),"URL not copied or is incorrect");		
+		Assert.assertTrue(copiedUrl.contains(url+"#/reports/topx?report_id="),"URL not copied or is incorrect");		
 		LOGGER.info("Url copied successfully and is navigable");
 		test.log(LogStatus.PASS, "Url copied successfully and is navigable");
 	}
-
 }
+

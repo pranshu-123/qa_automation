905ae1915d16ec9b7c7728aa95c0effcd6194a0a
Pranshu <pagarwal@unraveldata.com>
2023-02-02 20:14:57 +0530

    added jenkins system variables usage

--- src/main/java/com/qa/base/DriverManager.java
+++ src/main/java/com/qa/base/DriverManager.java
@@ -1,51 +1,53 @@
 package com.qa.base;
-	
 
-	import com.qa.io.ConfigReader;
-	import com.qa.utils.Log;
-	
 
-	import io.github.bonigarcia.wdm.WebDriverManager;
-	
+import com.qa.constants.SystemVariables;
+import com.qa.io.ConfigReader;
+import com.qa.utils.Log;
 
-	import org.apache.logging.log4j.LogManager;
-	import org.apache.logging.log4j.core.Logger;
-	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.chrome.ChromeDriver;
-	import org.openqa.selenium.chrome.ChromeOptions;
-	import org.openqa.selenium.logging.LogType;
-	import org.openqa.selenium.logging.LoggingPreferences;
-	import org.openqa.selenium.remote.RemoteWebDriver;
-	import java.net.MalformedURLException;
-	import java.net.URL;
-	import java.time.Duration;
-	import java.util.Properties;
-	import java.util.logging.Level;
-	
 
-	public class DriverManager {
-	
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-		WebDriver driver;
-		static Properties prop = ConfigReader.readBaseConfig();
-		static String executeOn = prop.getProperty("execution");
-	
 
-		public static final Logger log = (Logger) LogManager.getLogger();
-	
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.Logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.Properties;
+import java.util.logging.Level;
 
-		public WebDriver getDriver(String browser) {
-			if (driver == null) {
-				Log.info("Creating new driver **********");
-				driver = initializeDriver(browser);
-			}
-			return driver;
+
+public class DriverManager {
+
+
+	WebDriver driver;
+	static Properties prop = ConfigReader.readBaseConfig();
+	static String executeOn = prop.getProperty("execution");
+
+
+	public static final Logger log = (Logger) LogManager.getLogger();
+
+
+	public WebDriver getDriver(String browser) {
+		if (driver == null) {
+			Log.info("Creating new driver **********");
+			driver = initializeDriver(browser);
 		}
-	
+		return driver;
+	}
 
-		public WebDriver initializeDriver(String browser) {
-	
 
+	public WebDriver initializeDriver(String browser) {
+
+		String build_number = SystemVariables.BUILD_NUMBER.toString();
+		if(build_number!=null) {
 			if (browser.equalsIgnoreCase("chrome")) {
 				log.info("Using Chrome browser");
 				try {
@@ -56,25 +58,37 @@ package com.qa.base;
 			}
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+			String url = SystemVariables.ENVIRONMENT_URL.toString();
+			driver.get(url);
+		}
+		else {
+			driver = new ChromeDriver(getChromeOptionWithNetworkEnable());
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			String url = prop.getProperty("url");
 			driver.get(url);
-	
-
-			return driver;
 		}
-	
-
-		/**
-		 *
-		 * @return Chrome Options with customized configurations
-		 */
-		private ChromeOptions getChromeOptionWithNetworkEnable() {
-			LoggingPreferences logPrefs = new LoggingPreferences();
-			logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
-			System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
-			ChromeOptions chromeOptions = new ChromeOptions();
-			chromeOptions.addArguments("--no-sandbox", "--disable-dev-shm-usage");
-			chromeOptions.setExperimentalOption("useAutomationExtension", false);
-			return chromeOptions;
-		}
+		return driver;
 	}
+
+
+	/**
+	 *
+	 * @return Chrome Options with customized configurations
+	 */
+	private ChromeOptions getChromeOptionWithNetworkEnable() {
+		LoggingPreferences logPrefs = new LoggingPreferences();
+		logPrefs.enable(LogType.PERFORMANCE, Level.ALL);
+		String build_number = SystemVariables.BUILD_NUMBER.toString();
+		if(build_number!=null) {
+			System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		}
+		else {
+			WebDriverManager.chromedriver().setup();
+		}
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--no-sandbox", "--disable-dev-shm-usage");
+		chromeOptions.setExperimentalOption("useAutomationExtension", false);
+		return chromeOptions;
+	}
+}
